fbb718939bc2c23f72aae133ff8370935acefc1e
Admin <quangphanthanhnguyen@gmail.com>
2025-05-15 20:25:51 +0700

    upd

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getHome, getAdvise, getLogin, } = require('../controller/indexController.js');
+const { getHome, getAdvise, getLogin, getProfile } = require('../controller/indexController.js');
 const accountController = require('../controller/AccountController.js');
 router.get('/', getHome);
 router.get('/advise', getAdvise);
 router.get('/login', getLogin);
+router.get('/profile', getProfile);
 router.post('/register', accountController.createAccount);
 router.post('/login', accountController.loginAccount);
 router.post('/logout', accountController.xoaSessionUser);
